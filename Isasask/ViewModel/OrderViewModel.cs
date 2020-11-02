f0617067b3ecbef27da4ab12196aa80d74b7b04c
Ebram farag <60357766+Ebram3iad@users.noreply.github.com>
2020-11-02 14:58:15 +0200

    Adding ProductOrderViewModel

--- Isasask/ViewModel/OrderViewModel.cs
+++ Isasask/ViewModel/OrderViewModel.cs
@@ -1,9 +1,5 @@
 ﻿using Isasask.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Isasask.ViewModel
 {
     public class OrderViewModel
@@ -16,17 +12,5 @@ namespace Isasask.ViewModel
         public string SelectedCategory { get; set; }
         public int Quantity { get; set; }
 
-    }
-
-    public class ProductOrderViewModel
-    {
-        public int Id { get; set; }
-        public DateTime OrderDate { get; set; }
-        public int OrderNumber { get; set; }
-        public string ClientName { get; set; }
-        public string CategoryName { get; set; }
-        public string ProductName { get; set; }
-        public double Price { get; set; }
-        public int Quantity { get; set; }
     }
 }
---
+++ Isasask/ViewModel/ProductOrderViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Isasask.ViewModel
+{
+    public class ProductOrderViewModel
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int OrderNumber { get; set; }
+        public string ClientName { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
